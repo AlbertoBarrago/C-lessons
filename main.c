cff32cfcf808ce670848f61b16db737582ec665e
Alberto Barrago <albertobarrago@gmail.com>
2025-06-27 17:52:55 +0200

    fixup

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-//Global variable
+// Global variable
 int x = 0;
+float y = 1.234;
 
 int incr(int value) {
   x = x + value;
@@ -13,5 +14,10 @@ int main(void) {
   printf("Hello world\n");
   int value = 3;
   incr(3);
+  printf("%.3f\n", y);
+  //If we use unsigned the value will be 128 otherwhise -127
+  char c = 127;
+  c++;
+  printf("%d\n", c);
   return 0;
 }
