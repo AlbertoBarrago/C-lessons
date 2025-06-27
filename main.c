a4a20a95eb911a854952fa980e919b3fb25fb5e1
Alberto Barrago <albertobarrago@gmail.com>
2025-06-27 16:46:36 +0200

    ✨ feat: Introduces a new incr.

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
-int incr() {}
+//Global variable
+int x = 0;
+
+int incr(int value) {
+  x = x + value;
+  printf("%d\n", x);
+  return x;
+}
 
 int main(void) {
   printf("Hello world\n");
+  int value = 3;
+  incr(3);
   return 0;
 }
