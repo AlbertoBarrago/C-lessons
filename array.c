e25fe0db23a56886cdb380a375b95116760db439
Alberto Barrago <albertobarrago@gmail.com>
2025-06-30 16:29:56 +0200

    fix

--- array.c
+++ array.c
@@ -3,7 +3,8 @@
 int main(void) {
   int a[5] = {10, 5, 50, 100, 7};
   printf("%d\n", a[0]);
-  char str[6] = {'h','e','l','l','o', 0}; //zero is null terminator in decimal set
+  char str[6] = {'h', 'e', 'l',
+                 'l', 'o', 0}; // zero is null terminator in decimal set
   printf("%s\n", str);
   return 0;
 }
